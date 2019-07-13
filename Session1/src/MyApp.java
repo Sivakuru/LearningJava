137a3a8224cc73ffffb020f645b3952701c03ca1
sn185025 <sn185025@gmail.com>
2019-07-13 19:10:50 +0530

    Session 1 Introduction to Java

---
+++ Session1/src/MyApp.java
@@ -0,0 +1,15 @@
+
+public class MyApp {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Everyone");
+	}
+
+}
+
+class One 
+{
+	
+}
